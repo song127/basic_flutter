069557db05a7179230b2c241aa209bfc7e7cb2aa
song127 <deancontactadd@gmail.com>
2023-03-20 02:30:06 +0900

    init save

---
+++ lib/theme/global_colors.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+class GlobalColors {
+  static const Color trans = Color(0x00000000);
+
+  static const Color textColor1 = Color(0xFF212121);
+  static const Color textColor2 = Color(0xFF757575);
+
+  static const Color dividerColor = Color(0xFFBDBDBD);
+
+  static const Color iconColor = Color(0xFF757575);
+  static const Color iconColorDark = Color(0xFF212121);
+  static const Color iconColorLight = Color(0xFFBDBDBD);
+  static const Color iconColorWhite = Color(0xFFFFFFFF);
+  static const Color iconColorBlack = Color(0xFF222222);
+
+  static const Color transparentDark = Color(0x66000000);
+  static const Color transparentLight = Color(0x33FFFFFF);
+  static const Color transparentWhite = Color(0x66FFFFFF);
+  static const Color transparentBlack = Color(0x66000000);
+}
